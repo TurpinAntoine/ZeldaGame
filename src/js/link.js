23633eb19386cacd4a9558c6341a1e77fc4c7edb
Rayan94 <rayan.aittayeb@gmail.com>
2017-05-15 14:27:20 +0200

    Added JS

---
+++ src/js/link.js
@@ -0,0 +1,117 @@
+var link= function(name, posX, posY)
+{
+  
+  this.name = name;
+  this.posX = posX;
+  this.posY = posY;
+  this.cross = new Array();
+  this.transform = new Array();
+  this.div;
+  this.createLink = function()
+  
+  {
+    
+    this.div = document.createElement("div");
+    this.div.setAttribute("class", "zelda");
+    this.div.style.top = this.posY + "px";
+    this.div.style.left = this.posX + "px";
+    
+    this.afficherLink();
+    
+    
+  }
+  
+  this.afficherLink = function()
+  {
+    document.querySelector("body").appendChild(this.div);
+  }
+  
+  this.bougerLink = function()
+  {
+   var that = this; 
+    window.addEventListener('keypress', function (e) {
+      
+      
+    if(e.keyCode==that.cross[0])
+       {
+        
+        that.posY -= 10;
+         
+         if(e.keyCode==that.transform[1])
+        that.div.setAttribute("class", "zelda");
+        that.div.style.backgroundPosition = "0 -96px";
+        setTimeout(function(){ that.div.style.backgroundPosition = "-64px -96px" }, 200); 
+
+         
+       }
+  
+      
+    else if(e.keyCode==that.cross[1])
+      {
+        that.posX += 10;
+        if(e.keyCode==that.transform[1])
+        that.div.setAttribute("class", "zelda");
+        that.div.style.backgroundPosition = "0 -64px";
+        setTimeout(function(){ that.div.style.backgroundPosition = "-64px -64px" }, 200); 
+        
+      }
+        
+    else if(e.keyCode==that.cross[2])
+      {
+    
+        that.posY += 10;
+        if(e.keyCode==that.transform[1])
+        that.div.setAttribute("class", "zelda");
+        that.div.style.backgroundPosition = "0 0";
+        setTimeout(function(){ that.div.style.backgroundPosition = "-64px 0" }, 200); 
+       
+              
+      }
+    else if(e.keyCode==that.cross[3])
+      {
+        that.posX -= 10;
+        if(e.keyCode==that.transform[1])
+        that.div.setAttribute("class", "zelda");
+        that.div.style.backgroundPosition = "-64px -32px";
+        setTimeout(function(){ that.div.style.backgroundPosition = "0px -32px" }, 200);
+       
+      }
+      
+     else if(e.keyCode==that.transform[0])
+       
+       {
+         
+        that.div.setAttribute("class", "darkZelda");
+        
+       }
+      
+      if(e.keyCode==that.transform[1])
+       
+       {
+         
+        that.div.setAttribute("class", "zelda");
+        
+       }
+    
+      that.div.style.top = that.posY+"px";
+      that.div.style.left = that.posX+"px";
+}, false);
+ 
+    
+  }
+   
+}
+
+var zelda1 = new link("Pierre", 465, 265);
+zelda1.cross=[122,100,115,113];
+zelda1.transform=[101, 97];
+zelda1.createLink();
+zelda1.bougerLink();
+var zelda2 = new link("Paul", 67, 78);
+zelda2.createLink();
+zelda2.cross=[111,109,108,107];
+zelda2.bougerLink();
+var zelda3 = new link("Jacques", 120, 68);
+zelda3.createLink();
+zelda3.cross=[90,68,83,81];
+zelda3.bougerLink();
