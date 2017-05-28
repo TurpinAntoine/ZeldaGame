48a38d5955eae18dc0983048c9ca0e4999a874cb
Rayan94 <rayan.aittayeb@gmail.com>
2017-05-28 11:24:15 +0200

    Updated Map 2 with new features

--- src/js/map_boss.js
+++ src/js/map_boss.js
@@ -26,19 +26,6 @@ var maping = function () {
 var test = new maping()
 test.createTest();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 var posX = posX;
 var posY = posY;
 
@@ -52,8 +39,11 @@ var link = function (name, posX, posY) {
   this.cross = new Array();
   this.transform = new Array();
   this.div;
+  this.lifebar;
+  this.heart;
   this.attackDirection;
   this.life = 10;
+  this.walkDirection = 1;
 
   this.createLink = function () {
 
@@ -64,11 +54,68 @@ var link = function (name, posX, posY) {
 
     this.afficherLink();
 
-
   }
 
   this.afficherLink = function () {
     document.querySelector("body").appendChild(this.div);
+  }
+
+  this.linkLife = function () {
+
+    this.lifebar = document.createElement("div");
+    this.lifebar.setAttribute("class", "lifebar");
+    this.lifebar.style.position = "absolute";
+    this.lifebar.style.width = "100px";
+    this.lifebar.style.height = "10px";
+    this.lifebar.style.top = "0";
+    this.lifebar.style.right = "0";
+    this.lifebar.style.zIndex = "9";
+    this.lifebar.style.backgroundColor = "red";
+    this.displayLifeBar();
+
+  }
+
+  this.displayLifeBar = function() {
+
+    document.querySelector("body").appendChild(this.lifebar);
+  }
+
+  this.refreshLifebar = function(){
+
+      var that = this;
+      setInterval(function(){
+
+          that.lifebar.style.width = (that.life * 10) + "px";
+      }, 1)
+
+  }
+
+  this.createHeart = function(){
+
+    this.heart = document.createElement("div");
+    this.heart.setAttribute("class", "heart");
+    this.heart.style.top = Math.floor(Math.random() * 250) + "px";
+    this.heart.style.left = Math.floor(Math.random() * 800) + "px";
+    this.displayHeart();
+
+  }
+
+  this.displayHeart = function() {
+
+    document.querySelector("body").appendChild(this.heart);
+  }
+
+  this.addLife = function() {
+
+    var that = this;
+    setInterval(function(){
+
+      if (that.posX && that.posY == that.heart.style.top && that.heart.style.left)
+      {
+
+        console.log('LOOOURD')
+      }
+    }, 1)
   }
 
 
@@ -83,7 +130,18 @@ var link = function (name, posX, posY) {
           that.attackDirection = 1;
           if (e.keyCode == that.transform[1])
             that.div.setAttribute("class", "zelda");
-          that.div.style.backgroundPosition = "0 -96px";
+
+            if (this.walkDirection == 1){
+
+              that.div.style.backgroundPosition = "0 -96px";
+              this.walkDirection = 0;
+            }
+            else {
+
+              that.div.style.backgroundPosition = "-64px -96px";
+              this.walkDirection = 1;
+            }
+
         }
 
 
@@ -94,7 +152,19 @@ var link = function (name, posX, posY) {
           that.attackDirection = 2;
           if (e.keyCode == that.transform[1])
             that.div.setAttribute("class", "zelda");
-          that.div.style.backgroundPosition = "0 -64px";
+
+            if (this.walkDirection == 1){
+
+              that.div.style.backgroundPosition = "0 -64px";
+              this.walkDirection = 0;
+            }
+            else {
+
+              that.div.style.backgroundPosition = "-64px -64px";
+              this.walkDirection = 1;
+            }
+
+
         }
 
       } else if (e.keyCode == that.cross[2]) {
@@ -104,7 +174,19 @@ var link = function (name, posX, posY) {
           that.attackDirection = 3;
           if (e.keyCode == that.transform[1])
             that.div.setAttribute("class", "zelda");
-          that.div.style.backgroundPosition = "0 0";
+
+            if (this.walkDirection == 1){
+
+              that.div.style.backgroundPosition = "0 0";
+              this.walkDirection = 0;
+            }
+            else {
+
+              that.div.style.backgroundPosition = "-64px 0";
+              this.walkDirection = 1;
+            }
+
+
         }
 
 
@@ -114,7 +196,18 @@ var link = function (name, posX, posY) {
           that.attackDirection = 4;
           if (e.keyCode == that.transform[1])
             that.div.setAttribute("class", "zelda");
-          that.div.style.backgroundPosition = "-64px -32px";
+
+            if (this.walkDirection == 1){
+
+              that.div.style.backgroundPosition = "-64px -32px";
+              this.walkDirection = 0;
+            }
+            else {
+
+              that.div.style.backgroundPosition = "0 -32px";
+              this.walkDirection = 1;
+            }
+
 
 
         }
@@ -183,10 +276,6 @@ var link = function (name, posX, posY) {
 
   }
   this.attack = function () {
-
-
-
-
 
     var that = this;
     window.addEventListener('keypress', function (e) {
@@ -262,6 +351,21 @@ var link = function (name, posX, posY) {
     }, false);
   }
 
+  this.linkDespawn = function ( ){
+
+    setInterval(function(){
+
+      if (zelda1.life == 0){
+
+        if(confirm('You are dead ! Try again ?')){
+        window.location.reload();
+        }
+
+      }
+    }, 100)
+
+  }
+
 
 }
 
@@ -269,8 +373,13 @@ var zelda1 = new link("Pierre", 465, 265);
 zelda1.cross = [122, 100, 115, 113, 32];
 zelda1.transform = [101, 97];
 zelda1.createLink();
-//zelda1.createTest();
+zelda1.linkLife();
+zelda1.refreshLifebar();
+zelda1.linkDespawn();
 zelda1.bougerLink();
+zelda1.createHeart();
+zelda1.addLife();
+
 
 
 // Mobs
@@ -280,7 +389,7 @@ var monster = function (posX, posY) {
   this.posX = Math.floor(Math.random() * 800);
   this.posY = Math.floor(Math.random() * 250);
   this.div;
-  this.life = 1;
+  this.life = 3;
 
   this.createMob = function () {
 
@@ -290,19 +399,16 @@ var monster = function (posX, posY) {
     this.div.style.top = this.posY + "px";
     this.div.style.left = this.posX + "px";
     this.displayMob();
+
   }
 
   this.displayMob = function () {
 
     document.querySelector("body").appendChild(this.div);
 
-
   }
 
   this.moveMob = function () {
-
-
-
 
     var that = this;
 
@@ -311,12 +417,22 @@ var monster = function (posX, posY) {
       that.posX += Math.cos(Math.atan2(zelda1.posY - that.posY, zelda1.posX - that.posX)) * 2;
       that.posY += Math.sin(Math.atan2(zelda1.posY - that.posY, zelda1.posX - that.posX)) * 2;
       that.div.style.top = that.posY + "px";
-      that.div.style.left = that.posX + "px";
-
-
+      that.div.style.left = (that.posX - 25) + "px";
 
     }, 50);
   }
+
+
+  this.mobDespawn = function(){
+
+    console.log(monster1.life);
+    if (monster1.life == 2)(
+
+      console.log("lourd")
+    )
+  }
+
+
 
 
 }
@@ -325,6 +441,7 @@ var monster1 = new monster(this.posY, this.posX, this.life);
 monster1.createMob();
 monster1.displayMob();
 monster1.moveMob();
+monster1.mobDespawn();
 
 
 zelda1.attackDirection = [];
